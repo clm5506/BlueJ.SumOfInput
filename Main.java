98f834e635f479ad7c5df663abb3489a72454459
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-02-06 18:54:57 -0500

    Sum of input completed lab- Cristina

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner; 
 public class Main {
 
     public static void main(String[] args){
-
+        
+        Scanner myScanner = new Scanner(System.in);
+        int n;
+        int sum = 0;
+        System.out.println("Please input a number");
+        n = myScanner.nextInt();
+        for(int i = 0; i <= n; i++) {
+            sum = sum + i;
+        }
+        System.out.println("The sum is " + sum);
     }
 }
